a80ba31444ec9c2e7478116ad9426a6e80a4f0f2
bosslizhiyong <850895660@qq.com>
2019-10-30 16:01:24 +0800

    4

--- MyCRM.Web/Program.cs
+++ MyCRM.Web/Program.cs
@@ -19,7 +19,7 @@ namespace MyCRM.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();//1 
+                .UseStartup<Startup>();//13
 
     }
 }
